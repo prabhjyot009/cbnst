124c72d8fc4308a9317e000db7913a55812b613a
prabhjyot009 <prabh156dps@gmail.com>
2024-02-16 13:35:50 +0530

    Refactor error calculation in college/error.c

--- college/error.c
+++ college/error.c
@@ -2,17 +2,18 @@
 #include <conio.h>
 #include <math.h>
 
-int main(){
-    float ac,approx,err,abs,rel,per;
+int main()
+{
+    float ac, approx, err, abs, rel, per;
     printf("Enter the actual value: ");
-    scanf("%f",&ac);
+    scanf("%f", &ac);
     printf("Enter the approximate value: ");
-    scanf("%f",&approx);
-    err=ac-approx;
-    abs=fabs(err);
-    rel=abs/ac;
-    per=rel*100;
-    printf("Error: %f\nAbsolute Error: %f\nRelative Error: %f\nPercentage Error: %f",err,abs,rel,per);
+    scanf("%f", &approx);
+    err = ac - approx;
+    abs = fabs(err);
+    rel = abs / ac;
+    per = rel * 100;
+    printf("Error: %f\nAbsolute Error: %f\nRelative Error: %f\nPercentage Error: %f", err, abs, rel, per);
     getch();
     return 0;
 }
