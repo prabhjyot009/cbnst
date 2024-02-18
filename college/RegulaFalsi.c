0ef6058985fda912c0a7e31d7cb894fb1989c3f1
prabhjyot009 <prabh156dps@gmail.com>
2024-02-18 16:35:35 +0530

    Refactor RegulaFalsi.c: Improve code structure and optimize calculations

--- college/RegulaFalsi.c
+++ college/RegulaFalsi.c
@@ -1,55 +1,38 @@
 #include <stdio.h>
 #include <conio.h>
 #include <math.h>
-
 double eq(double x)
 {
     double f;
     f = (x * x * x) - (4 * x) - 9;
     return (f);
 }
-void find_intervals(double *a, double *b)
+double formula(double a, double b, double f1, double f2)
 {
-    double f1, f2;
-    int i;
-    for (i = 0; i <= 10; i++)
-    {
-        f1 = eq(i);
-        f2 = eq(i + 1);
-        if ((f1 < 0 && f2 > 0) || (f1 > 0 && f2 < 0))
-        {
-            printf("Intervals are:\t%d, %d\n", i, i + 1);
-            *a = i;
-            *b = i + 1;
-        }
-    }
-}
-void regula_falsi(double a, double b, float range)
-{
-    double z, c, f1, f2;
-    int it;
-    for (it = 1; fabs(a - b) >= range; it++)
-    {
-        printf("Iteration:%d\t", it);
-        f1=eq(a);
-        f2=eq(b);
-        printf("a:%f\tb:%f\t", a, b);
-        c = ((a * f2) - (b * f1)) / (f2 - f1);
-        printf("c:%f\n\n", c);
-        z = eq(c);
-        if (z == 0.0)
-            break;
-        else if (z * f1 < 0)
-            b = c;
-        else
-            a = c;
-    }
+    double z;
+    z = (((a * f2) - (b * f1)) / (f2 - f1));
+    return (z);
 }
 int main()
 {
-    double a, b;
-    float range = 0.000001;
-    find_intervals(&a, &b);
-    regula_falsi(a, b, range);
+    double f3, c, f1, f2, a=2, b=3;
+    int i = 1;
+    do
+    {
+        printf("Iteration:%d\t", i);
+        printf("a:%f\tb:%f\t", a, b);
+        f1 = eq(a);
+        printf("f(a):%f\t", f1);
+        f2 = eq(b);
+        printf("f(b):%f\t\n", f2);
+        c = formula(a, b, f1, f2);
+        f3 = eq(c);
+        if (f3 > 0)
+            b = c;
+        else
+            a = c;
+        i++;
+    } while (fabs(f3) > 0.0005);
+    printf("approx is:%f", c);
     return 0;
 }
