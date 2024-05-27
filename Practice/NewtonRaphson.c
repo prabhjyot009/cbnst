34c0bfb6ee94fc95d2bcf4be5a2ed1745f3151c4
prabhjyot009 <prabhjyot009s@gmail.com>
2024-05-27 23:40:09 +0530

    Newton Raphson

---
+++ Practice/NewtonRaphson.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define f(x) x*sin(x)+cos(x);
+#define g(x) x*cos(x)+sin(x);
+
+int main(){
+    float x0=3.14159,x1,f0,f1,g0,range=0.0001;
+    int steps=1;
+    do{
+        f0=f(x0);
+        g0=g(x0);
+        x1=x0-(f0/g0);
+        printf("step%d\tx0:%f\tf0:%f\tx1:%f\tf1:%f\n", steps, x0, f0, x1, f1);
+        x0=x1;
+        f1=f(x1);
+        steps++;
+    }while(fabs(f1)>range);
+    printf("Root is:%f", x1);
+    return 0;
+}
