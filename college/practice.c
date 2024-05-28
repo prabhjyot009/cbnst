55962884009f29bdb5ec493733bcfaae54566bfe
prabhjyot009 <prabhjyot009s@gmail.com>
2024-05-28 18:39:12 +0530

    refactor: Improve code for calculating interpolation using Newton's backward difference formula

--- college/practice.c
+++ college/practice.c
@@ -1,43 +1,41 @@
 #include<stdio.h>
-#include<conio.h>
 #include<math.h>
 
 int main(){
-    float a[100][100],x,y,u1,u;
-    int i,j,fact,n;
-    printf("Enter the number of terms:\n");
+    float a[100][100],x,u1,u,y,fact;
+    int i,j,n;
+    printf("Enter number of terms:");
     scanf("%d",&n);
-    printf("Enter the values for x:\n");
+    printf("Enter x values:\n");
     for(i=0;i<n;i++){
         scanf("%f",&a[i][0]);
     }
-    printf("Enter the values for x:\n");
+    printf("Enter y values:\n");
     for(i=0;i<n;i++){
         scanf("%f",&a[i][1]);
     }
-    printf("Enter the value for f(x):\n");
-    scanf("%d",&x);
-    //diff table:
+    printf("Enter the value of x you want y:");
+    scanf("%f",&x);
     for(j=2;j<n+1;j++){
         for(i=0;i<n-j+1;i++){
             a[i][j]=a[i+1][j-1]-a[i][j-1];
         }
     }
-    //print
+    printf("Difference table");
     for(i=0;i<n;i++){
         for(j=0;j<=n-i;j++){
             printf("%f",a[i][j]);
         }
         printf("\n");
     }
-    //find u
-    u=(x-a[0][0])/(a[1][0]-a[0][0]);
-    y=a[0][1];
+    u=x-a[0][0]/(a[1][0]-a[0][0]);
     u1=u;
+    y=a[0][1];
+    fact=1;
     for(i=2;i<=n;i++){
-        y=y+(u1*a[0][1])/fact;
+        y=y+(u1*a[0][i])/fact;
         fact*=i;
-        u1+=u-(i-1);
+        u1=u1*u-(i-1);
     }
     printf("\n\nValue at X=%g is = %f",x,y);
 }
